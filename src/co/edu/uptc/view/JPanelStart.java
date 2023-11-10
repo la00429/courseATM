3a41e1aaae1aea8a5b4a598ed2258d16d57e0f91
lau00429 <laura.figueredo02@uptc.edu.co>
2023-11-10 10:40:22 -0500

    panel Start modificado

--- src/co/edu/uptc/view/JPanelStart.java
+++ src/co/edu/uptc/view/JPanelStart.java
@@ -1,5 +1,24 @@
 package co.edu.uptc.view;
 
-public class JPanelStart {
+import java.awt.Dimension;
+import java.awt.event.ActionListener;
+
+import javax.swing.JPanel;
+
+public class JPanelStart extends JPanel {
+	private JPanel imgUser;
+	private JPanel infoPanel;
+	private JButtonUPTC button;
+
+	public JPanelStart(ActionListener listener) {
+		this.setSize(getMaximumSize());
+		initComponents(listener);
+		this.setVisible(true);
+
+	}
+
+	public static void main(String[] args) {
+		new 
+	}
 
 }
