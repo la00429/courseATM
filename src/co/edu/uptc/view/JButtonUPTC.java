4af86ebbf048046401b1b86dc6a06dee0b03e3c7
lau00429 <laura.figueredo02@uptc.edu.co>
2023-11-10 10:28:34 -0500

    add JPanelStart (H) and Create JButton UPTC

---
+++ src/co/edu/uptc/view/JButtonUPTC.java
@@ -0,0 +1,44 @@
+package co.edu.uptc.view;
+
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+
+import javax.swing.JButton;
+import javax.swing.border.EmptyBorder;
+
+public class JButtonUPTC extends JButton {
+	private static final long serialVersionUID = 1L;
+
+	public JButtonUPTC(String text) {
+		super(text);
+		setFocusPainted(false);
+		setContentAreaFilled(false);
+		setBackground(Color.GRAY);
+		setForeground(Color.WHITE);
+		setFocusPainted(false);
+		setFont(new Font("Arial", Font.BOLD, 16));
+		setBorderPainted(false);
+		setBorder(new EmptyBorder(10, 10, 10, 10));
+	}
+
+	@Override
+	protected void paintComponent(Graphics g) {
+		if (getModel().isArmed()) {
+			g.setColor(Color.lightGray);
+		} else {
+			g.setColor(getBackground());
+		}
+		((Graphics2D) g).setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+		g.fillRoundRect(0, 0, getWidth(), getHeight(), 20, 20);
+		super.paintComponent(g);
+	}
+
+	@Override
+	protected void paintBorder(Graphics g) {
+		g.setColor(getForeground());
+		g.drawRoundRect(0, 0, getWidth() - 1, getHeight() - 1, 20, 20);
+	}
+}
