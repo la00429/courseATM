58f951b6113d0b8db9059c3f3b373c67f250f54a
Tsuju8 <laura.moreno22@uptc.edu.co>
2023-11-14 11:01:25 -0500

    cambios course getandsetters

--- src/co/edu/uptc/model/Course.java
+++ src/co/edu/uptc/model/Course.java
@@ -3,12 +3,30 @@ package co.edu.uptc.model;
 public class Course {
 	private String name;
 	private String info;
-	
+		
 	public Course(String name, String info) {
 		super();
 		this.name = name;
 		this.info = info;
 	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getInfo() {
+		return info;
+	}
+
+	public void setInfo(String info) {
+		this.info = info;
+	}
+	
+	
 	
 	
 	
