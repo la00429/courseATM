f1eaadcb8075745410b98b0211efd4c5cb0815fc
Laura Figueredo <112426346+la00429@users.noreply.github.com>
2023-11-22 16:24:36 -0500

    add entegrable

--- src/co/edu/uptc/view/WebCourse.java
+++ src/co/edu/uptc/view/WebCourse.java
@@ -9,8 +9,8 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.FlowPane;
-
 import javafx.scene.web.WebView;
+import javafx.stage.Stage;
 
 // TODO: Auto-generated Javadoc
 /**
@@ -18,7 +18,7 @@ import javafx.scene.web.WebView;
  * zoom. Extiende JFXPanel y contiene un WebView para mostrar contenido web y
  * botones para controlar el zoom.
  */
-public class WebCourse extends JFXPanel {
+public class WebCourse extends JFXPanel{
 
 	/**  SerialVersionUID para la serialización de la clase. */
 	private static final long serialVersionUID = 1L;
@@ -40,16 +40,6 @@ public class WebCourse extends JFXPanel {
 	 */
 	private void initializeWebView(String path) {
 		webView = new WebView();
-		webView.getEngine().load(path);
-
-		zoomInButton = new Button("Zoom In");
-		zoomOutButton = new Button("Zoom Out");
-		zoomInButton.setOnAction(e -> changeZoom(webView, 0.1));
-		zoomOutButton.setOnAction(e -> changeZoom(webView, -0.1));
-
-		webView = new WebView();
-		webView.getEngine().load(path);
-
 		zoomInButton = new Button("Zoom In");
 		zoomOutButton = new Button("Zoom Out");
 		zoomInButton.setOnAction(e -> changeZoom(webView, 0.1));
